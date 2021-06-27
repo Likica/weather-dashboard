ab290ed990e81a3aa8b209902caa7fdab8b24451
Coco Puff <fahrija_wyroski@live.com>
2021-06-27 13:06:08 -0500

    set up

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,56 +1,19 @@
-// Tutorial by http://youtube.com/CodeExplained
-// api key : 96739db56d8d475dad049fca49fa349e
+//declare variables
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#city");
+var weatherContainerEl = document.querySelector("#weather-container");
+var forecastSearchTerm = document.querySelector("#forecast-search-term");
 
-// SELECT ELEMENTS
-const iconElement = document.querySelector(".weather-icon");
-const tempElement = document.querySelector(".temperature-value p");
-const descElement = document.querySelector(".temperature-description p");
-const locationElement = document.querySelector(".location p");
-const notificationElement = document.querySelector(".notification");
+// My API key : 96739db56d8d475dad049fca49fa349e
+// <!--my API key: 96739db56d8d475dad049fca49fa349e-->
+// <!-- <link
+//     href="https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,alerts&appid=96739db56d8d475dad049fca49fa349e$"
+//     rel="stylesheet" /> -->
+// fetch(url) method
+fetch(url)
+    .then(function () {
 
-// App data
-const weather = {};
+    })
+    .catch(function () {
 
-weather.temperature = {
-    unit: "Fahrenheit"
-}
-// API KEY
-// const apiKey = "96739db56d8d475dad049fca49fa349e";
-
-// SET USER'S POSITION
-function setPosition(position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-
-    getWeather(latitude, longitude);
-    console.log(position);
-}
-
-// GET WEATHER FROM API PROVIDER
-function getWeather(latitude, longitude) {
-    let apiKey = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,alerts&appid=96739db56d8d475dad049fca49fa349e$`;
-
-    fetch(api)
-        .then(function (response) {
-            let data = response.json();
-            return data;
-        })
-        .then(function (data) {
-            weather.temperature.value = Math.floor(data.main.temp - weather);
-            weather.description = data.weather[0].description;
-            weather.iconId = data.weather[0].icon;
-            weather.city = data.name;
-            weather.country = data.sys.country;
-        })
-        .then(function () {
-            displayWeather();
-        });
-}
-
-// DISPLAY WEATHER TO UI
-function displayWeather() {
-    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
-    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-    descElement.innerHTML = weather.description;
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
-};
+    });
