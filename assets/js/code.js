373bc6d3e10fc2cae9094704cc9c2c6a930e0805
Coco Puff <fahrija_wyroski@live.com>
2021-06-27 15:57:26 -0500

    api add

--- assets/js/code.js
+++ assets/js/code.js
@@ -2,8 +2,8 @@
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
 var weatherContainerEl = document.querySelector("#weather-container");
-var forecastContainer = document.querySelector("#forecast-container");
-var searchHistory = document.querySelector("#history-container")
+var forecastContainerEl = document.querySelector("#forecast-container");
+var searchHistoryEl = document.querySelector("#history-container")
 var myAPIkey = `96739db56d8d475dad049fca49fa349e`
 
 // my API key: 96739db56d8d475dad049fca49fa349e
@@ -11,6 +11,19 @@ var myAPIkey = `96739db56d8d475dad049fca49fa349e`
 //     href="https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,alerts&appid=96739db56d8d475dad049fca49fa349e$"
 
 // fetch(url) method
+//var getFeaturedRepos = function(language) {
+//     var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+//     fetch(apiUrl).then(function(response) {
+//       if (response.ok) {
+//         response.json().then(function(data) {
+//           displayRepos(data.items, language);
+//         });
+//       } else {
+//         alert('Error: GitHub User Not Found');
+//       }
+//     });
+//   };
 fetch(url)
     .then(function () {
 
