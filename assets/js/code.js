cdd72b1cad6bf2e796e9aefd3d3deb3d8bcb992f
Coco Puff <fahrija_wyroski@live.com>
2021-06-27 15:14:38 -0500

    html adjust

--- assets/js/code.js
+++ assets/js/code.js
@@ -2,14 +2,14 @@
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
 var weatherContainerEl = document.querySelector("#weather-container");
-var forecastSearchTerm = document.querySelector("#forecast-search-term");
+var forecastContainer = document.querySelector("#forecast-container");
 var searchHistory = document.querySelector("#history-container")
+var myAPIkey = `96739db56d8d475dad049fca49fa349e`
 
-// My API key : 96739db56d8d475dad049fca49fa349e
-// <!--my API key: 96739db56d8d475dad049fca49fa349e-->
-// <!-- <link
+// my API key: 96739db56d8d475dad049fca49fa349e
+// <link
 //     href="https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,alerts&appid=96739db56d8d475dad049fca49fa349e$"
-//     rel="stylesheet" /> -->
+
 // fetch(url) method
 fetch(url)
     .then(function () {
