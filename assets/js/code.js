6b9339ff971b6541aa8b8bc9e08783f40bb3d505
Coco Puff <fahrija_wyroski@live.com>
2021-06-27 21:43:22 -0500

    js functions, html elements

--- assets/js/code.js
+++ assets/js/code.js
@@ -2,32 +2,111 @@
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
 var weatherContainerEl = document.querySelector("#weather-container");
-var forecastContainerEl = document.querySelector("#forecast-container");
-var searchHistoryEl = document.querySelector("#history-container")
+var forecastContainer = document.querySelector("#forecast-container");
+var searchHistory = document.querySelector("#history-container")
+var search = document.querySelector("#search");
+var htmlDate = document.querySelector("#date");
+var htmlCity = document.querySelector("#city");
+var htmlIcon = document.querySelector("#icon");
+var htmlWind = document.querySelector("#wind");
+var htmlUvi = document.querySelector("#uvi");
+var htmlHumid = document.querySelector("#humidity");
+var htmlTemp = document.querySelector("#temp");
 var myAPIkey = `96739db56d8d475dad049fca49fa349e`
+search.addEventListener("click", handleSearchSubmit);
 
-// my API key: 96739db56d8d475dad049fca49fa349e
-// <link
-//     href="https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,alerts&appid=96739db56d8d475dad049fca49fa349e$"
 
-// fetch(url) method
-//var getFeaturedRepos = function(language) {
-//     var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+function handleSearchSubmit(e) {
+    e.preventDefault();
+    var cityName = cityInputEl.value;
+    console.log("city", cityName);
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${myAPIkey}`;
+    fetch(apiUrl)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            //render that info
+            console.log(data);
+            var info = {
+                lat: data.city.coord.lat,
+                lon: data.city.coord.lon,
+                city: data.city.name,
+                date: data.list[0].dt_txt
+            }
+            getWeather(info);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+function getWeather(info) {
+    var lat = info.lat;
+    var lon = info.lon;
+    var cityName = info.city;
+    var date = info.date;
+    console.log("cityName", info);
+    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${myAPIkey}`;
+    fetch(apiUrl)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            //render that info
+            console.log(data);
+            renderInfo(data, cityName, date);
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
+function renderInfo(info, cityName, date) {
+    console.log("info received", info)
+    // <!--Need results of the search here: current-day weather, city-name, current-date, weather-icon, temperature-now, wind-now, humidity-now, UV-index-->
+    var temp = info.current.temp;
+    var city = cityName;
+    var day = date;
+    htmlDate.innerHTML = day;
+    htmlCity.innerHTML = city;
+    htmlIcon.innerHTML = info.current.weather[0].icon;
+    htmlWind.innerHTML = info.current.wind_speed;
+    htmlUvi.innerHTML = info.current.uvi;
+    htmlHumid.innerHTML = info.current.humidity;
+    htmlTemp.innerHTML = temp;
+}
+// forecast
+function getForecast(info) {
+    var lat = info.lat;
+    var lon = info.lon;
+    var cityName = info.city;
+    var date = info.date;
+    console.log("cityName", info);
+    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${myAPIkey}`;
+    fetch(apiUrl)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            //render that info
+            console.log(data);
+            renderInfo(data, cityName, date);
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
+function renderInfo(info, cityName, date) {
+    console.log("info received", info)
+    // <!--Need results of the search here: current-day weather, city-name, current-date, weather-icon, temperature-now, wind-now, humidity-now, UV-index-->
+    var temp = info.current.temp;
+    var city = cityName;
+    var day = date;
+    htmlDate.innerHTML = day;
+    htmlCity.innerHTML = city;
+    htmlIcon.innerHTML = info.current.weather[0].icon;
+    htmlWind.innerHTML = info.current.wind_speed;
+    htmlUvi.innerHTML = info.current.uvi;
+    htmlHumid.innerHTML = info.current.humidity;
+    htmlTemp.innerHTML = temp;
+}
 
-//     fetch(apiUrl).then(function(response) {
-//       if (response.ok) {
-//         response.json().then(function(data) {
-//           displayRepos(data.items, language);
-//         });
-//       } else {
-//         alert('Error: GitHub User Not Found');
-//       }
-//     });
-//   };
-fetch(url)
-    .then(function () {
-
-    })
-    .catch(function () {
-
-    });
